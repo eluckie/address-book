3e50d9b93e57ac74deeb229268e33a30eb79b8ac
E Luckie <eluckie.d@gmail.com>
2023-01-22 19:07:02 -0700

    add script.js file with initial AddressBook and Contact constructors

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// business logic for AddressBook
+function AddressBook() {
+  this.contacts = {};
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+//business logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
